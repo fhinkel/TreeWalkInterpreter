69c9d2d60428130852fc8a00199d5b9526cd6ad0
Franziska Hinkelmann <franziska.hinkelmann@gmail.com>
2024-07-31 20:17:01 -0400

    statements

--- tool/GenerateAst.java
+++ tool/GenerateAst.java
@@ -18,6 +18,9 @@ public class GenerateAst {
                 "Grouping : Expr expression",
                 "Literal : Object value",
                 "Unary : Token operator, Expr right"));
+        defineAst(outputDir, "Stmt", Arrays.asList(
+                "Expression : Expr expression",
+                "Print: Expr expression"));
 
     }
 
